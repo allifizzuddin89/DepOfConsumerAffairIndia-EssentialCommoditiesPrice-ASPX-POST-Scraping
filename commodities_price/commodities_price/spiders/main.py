7be935f1b894d58a95707ca13cca71e20748b83b
allifizzuddin89 <allifizzuddin@gmail.com>
2023-02-01 13:59:17 +0800

    Complete beta

--- commodities_price/commodities_price/spiders/main.py
+++ commodities_price/commodities_price/spiders/main.py
@@ -4,6 +4,13 @@ from scrapy.shell import inspect_response
 from scrapy.utils.response import open_in_browser
 import pandas as pd
 
+# proxy
+# import sys
+# sys.path.insert(0, '/home/allifizzuddin/Documents/Freelance job/Portfolio/DepOfConsumerAffairIndia-EssentialCommoditiesPrice-ASPX-POST-Scraping/commodities_price/commodities_price/spiders')
+# from creds import API
+# from scraper_api import ScraperAPIClient
+# client = ScraperAPIClient(API)
+
 # Logging DEBUG
 import logging
 logger = logging.getLogger('my_logger')
@@ -15,9 +22,10 @@ logging.basicConfig(filename="logfile.txt",
 class MainSpider(scrapy.Spider):
     name = 'main'
     #allowed_domains = ['x']
-    #start_urls = ['http://x/']
+    # start_urls = [client.scrapyGet(url ='https://fcainfoweb.nic.in/reports/report_menu_web.aspx')]
 
-    url = 'https://fcainfoweb.nic.in/reports/report_menu_web.aspx'
+    start_urls = ['https://fcainfoweb.nic.in/reports/report_menu_web.aspx']
+    # url = 'https://fcainfoweb.nic.in/reports/report_menu_web.aspx'
 
     headers = {
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -48,17 +56,52 @@ class MainSpider(scrapy.Spider):
 
     
 
-    def start_requests(self):
-        yield scrapy.Request(
-            url=self.url,
-            method='POST',
-            dont_filter=True,
-            cookies=self.cookies,
-            headers=self.headers,
-            body=self.body,
-        )
+    # def start_requests(self):
+    #     yield scrapy.Request(
+    #         # client.scrapyGet(url=self.url),
+    #         url=self.url,
+    #         method='POST',
+    #         dont_filter=True,
+    #         cookies=self.cookies,
+    #         headers=self.headers,
+    #         body=self.body,
+    #     )
 
     def parse(self, response):
+        formdata = {
+            "ctl00$MainContent$Ddl_Rpt_type": "Retail",
+            "ctl00$MainContent$ddl_Language": "English",
+            "ctl00$MainContent$Rbl_Rpt_type": "Price report",
+            # "ctl00$MainContent$Ddl_Rpt_Option0": "Daily Prices",
+            # "ctl00$MainContent$Txt_FrmDate": "30/01/2023",
+            # "ctl00$MainContent$btn_getdata1": "Get Data",
+        }
+
+        yield FormRequest.from_response(
+            response=response,
+            formdata=formdata,
+            # headers=self.headers,
+            callback=self.step2
+        )
+    
+    def step2(self, response):
+        formdata = {
+            "ctl00$MainContent$Ddl_Rpt_type": "Retail",
+            "ctl00$MainContent$ddl_Language": "English",
+            "ctl00$MainContent$Rbl_Rpt_type": "Price report",
+            "ctl00$MainContent$Ddl_Rpt_Option0": "Daily Prices",
+            # "ctl00$MainContent$Txt_FrmDate": "30/01/2023",
+            # "ctl00$MainContent$btn_getdata1": "Get Data",
+        }
+
+        yield FormRequest.from_response(
+            response=response,
+            formdata=formdata,
+            # headers=self.headers,
+            callback=self.step3
+        )
+    
+    def step3(self, response):
         formdata = {
             "ctl00$MainContent$Ddl_Rpt_type": "Retail",
             "ctl00$MainContent$ddl_Language": "English",
@@ -71,9 +114,12 @@ class MainSpider(scrapy.Spider):
         yield FormRequest.from_response(
             response=response,
             formdata=formdata,
-            headers=self.headers,
+            # headers=self.headers,
             callback=self.parse_table
         )
     
     def parse_table(self, response):
-        open_in_browser(response)
+        # open_in_browser(response)
+        dfs = pd.read_html(response.text)
+        for i, df in enumerate(dfs):
+            df.to_csv('Commodity_price_{}'.format(i))
